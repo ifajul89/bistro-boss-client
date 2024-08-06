764a6cb7dbc5a505074faf410618c2dcd5aeb5d2
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-08-07 01:49:57 +0600

    added basic navbar item

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
+import NavBar from "../Pages/Shared/NavBar/NavBar";
 
 const Main = () => {
-   return (
-      <>
-         <Outlet></Outlet>
-         <Footer></Footer>
-      </>
-   );
+  return (
+    <>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default Main;
