1f0201980cddfe6fd561e4bbeb060cebcfc052e3
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-08-06 13:16:18 +0600

    added footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 const Main = () => {
-    return (
-        <Outlet></Outlet>
-    );
+   return (
+      <>
+         <Outlet></Outlet>
+         <Footer></Footer>
+      </>
+   );
 };
 
 export default Main;
